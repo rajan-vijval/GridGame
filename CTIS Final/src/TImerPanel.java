998c448cf4a19683b4e9c07b2b6d0406bd8b7487
nimbostruck14 <nwonchalet@gmail.com>
2016-11-28 23:58:41 -0500

    Add files via upload

---
+++ CTIS Final/src/TImerPanel.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class TImerPanel extends JPanel implements ActionListener
+//this sets up the timer so it counts from 100 and counts by seconds
+{
+	JLabel setTime = new JLabel();
+//	JLabel score = new JLabel();
+	int time = 100;
+	Timer setT = new Timer(1000,this);
+	//final int COUNT = 0;
+
+	//----------------------------------------------
+	//Constructor: Sets up timer Panel
+	//----------------------------------------------
+
+	public TImerPanel ()
+	{
+		setBackground(Color.cyan);
+		setPreferredSize(new Dimension(400,400));
+	
+		setFont(new Font("Verdana", Font.BOLD, 22));
+		add(setTime);
+		setT.start();
+	//	add(score);
+		setVisible(true);
+	}
+	//This creates the logic for the timer
+	public void logic()
+	{
+		time--;
+		setTime.setText(""+time);
+		
+		//if (incorrect square clicked)
+		//{
+		//	time-3;
+		//	setTime.setText(""+time);
+		// else		null
+		//}
+	}
+	//public void logic2()
+	//{
+	//	if(correct square is clicked)
+	//	count++;
+	//  else
+	//		null
+	//}
+	//implements the logic for the timer
+	public void actionPerformed(ActionEvent e)
+	{
+		logic();
+		//logic2();
+		repaint();
+	}
+}
