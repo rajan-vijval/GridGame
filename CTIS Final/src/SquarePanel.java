9d42fe1ebeff5ffadd689e4bd518dcc5b477f945
Vijval Rajan <rajan-vijval@users.noreply.github.com>
2016-11-29 21:30:48 -0500

    Update SquarePanel.java

--- CTIS Final/src/SquarePanel.java
+++ CTIS Final/src/SquarePanel.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class SquarePanel extends JPanel
@@ -6,8 +7,21 @@ public class SquarePanel extends JPanel
 	/*
 	 * Creating a class for individual square on the grid
 	 */
+	
+	private JButton squareButton;
+	
 	public SquarePanel()
 	{
-		JPanel subPanel = new JPanel();
+		squareButton = new JButton();
+		squareButton.addActionListener(new ButtonListener());
+	}
+	
+	private class ButtonListener implements ActionListener
+	{
+		
+		public void actionPerformed(ActionEvent event)
+		{
+			//whatever we need to do with the score thing
+		}
 	}
 }
