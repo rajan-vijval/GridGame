96498c4dcadd09b665708122fd9f5e79d349ba0b
Vijval Rajan <rajan-vijval@users.noreply.github.com>
2016-11-29 22:09:57 -0500

    Update TimerFrame.java

--- CTIS Final/src/TimerFrame.java
+++ CTIS Final/src/TimerFrame.java
@@ -5,7 +5,7 @@ public class TimerFrame extends JFrame{
 //Constructor: this sets up the entire JFrame for the timer
 	public TimerFrame ()
 {
-	add(new TImerPanel());
+	add(new TimerPanel());
 	setSize(new Dimension(400,400));
 	setTitle("Timer");
 	setVisible(true);
